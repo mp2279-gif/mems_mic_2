2d0c55a60c5796599b8de242b2bb3a4652351873
mp2279-gif <mp2279@cornell.edu>
2026-01-05 18:00:11 -0500

    Update pico_sdk_import.cmake

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -2,7 +2,7 @@
 
 # This can be dropped into an external project to help locate this SDK
 # It should be include()ed prior to project()
-"""
+'''
 if (DEFINED ENV{PICO_SDK_PATH} AND (NOT PICO_SDK_PATH))
     set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
     message("Using PICO_SDK_PATH from environment ('${PICO_SDK_PATH}')")
@@ -67,7 +67,7 @@ set(PICO_SDK_INIT_CMAKE_FILE ${PICO_SDK_PATH}/pico_sdk_init.cmake)
 if (NOT EXISTS ${PICO_SDK_INIT_CMAKE_FILE})
     message(FATAL_ERROR "Directory '${PICO_SDK_PATH}' does not appear to contain the Raspberry Pi Pico SDK")
 endif ()
-
+'''
 set(PICO_SDK_PATH ${PICO_SDK_PATH} CACHE PATH "Path to the Raspberry Pi Pico SDK" FORCE)
 
 include(${PICO_SDK_INIT_CMAKE_FILE})
