073423573b7b8c2d12b9f18cdccc9afcfa254862
mp2279-gif <mp2279@cornell.edu>
2026-01-07 11:51:06 -0500

    Update main.c

--- examples/usb_microphone/main.c
+++ examples/usb_microphone/main.c
@@ -17,7 +17,7 @@
 #include "usb_microphone.h"
 
 #include "analog_microphone.h"
-#include "pdm_microphone.h"
+//#include "pdm_microphone.h"
 
 void on_analog_samples_ready(void); // Forward declaration
 
