61f93921d6ce7cc15dd7203237b2cb451be77316
mp2279-gif <mp2279@cornell.edu>
2026-01-07 23:11:00 -0500

    Update usb_microphone.h

--- examples/usb_microphone/usb_microphone.h
+++ examples/usb_microphone/usb_microphone.h
@@ -11,11 +11,13 @@
 #include "tusb.h"
 
 #ifndef SAMPLE_RATE
-#define SAMPLE_RATE ((CFG_TUD_AUDIO_EP_SZ_IN / 2) - 1) * 1000
+#define SAMPLE_RATE (48000) //48 kHz
+//#define SAMPLE_RATE ((CFG_TUD_AUDIO_EP_SZ_IN / 2) - 1) * 1000
 #endif
 
 #ifndef SAMPLE_BUFFER_SIZE
-#define SAMPLE_BUFFER_SIZE ((CFG_TUD_AUDIO_EP_SZ_IN/2) - 1)
+#define SAMPLE_BUFFER_SIZE (CFG_TUD_AUDIO_EP_SZ_IN / CFG_TUD_AUDIO_FUNC_1_N_BYTES_PER_SAMPLE_TX)
+//#define SAMPLE_BUFFER_SIZE ((CFG_TUD_AUDIO_EP_SZ_IN/2) - 1)
 #endif
 
 typedef void (*usb_microphone_tx_ready_handler_t)(void);
