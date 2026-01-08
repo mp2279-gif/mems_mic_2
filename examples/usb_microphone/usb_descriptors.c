cfa194d8f393ea00687be62cd7758d068f21b43f
mp2279-gif <mp2279@cornell.edu>
2026-01-08 15:14:37 -0500

    Update usb_descriptors.c

--- examples/usb_microphone/usb_descriptors.c
+++ examples/usb_microphone/usb_descriptors.c
@@ -23,8 +23,7 @@
  *
  */
 
-#include "tusb.h" //this is part of TinyUSB
-//#include "tusb_config.h"
+#include "tusb.h"
 
 /* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
  * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
